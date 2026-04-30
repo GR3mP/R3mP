ae3e53706b262b79ec2f0dfa7685670e65dccb7e
Gleb Obitotsky <pluginexteragram@gmail.com>
2026-04-30 17:16:20 +1000

    update

--- utils/r3mp/utils/BigNum.cpp
+++ utils/r3mp/utils/BigNum.cpp
@@ -2,3 +2,13 @@
 //
 // The code is distributed under the GNU GPL v.3 license 
 // (you can find the license file in the root folder).
+
+#include "BigNum.h"
+
+namespace r3mp {
+
+class BigNumContext::Impl {
+ public:
+};
+
+} // namespace r3mp
