5c0f103224baf06cc7c4292db03b82d48c542922
Gleb Obitotsky <pluginexteragram@gmail.com>
2026-05-03 22:16:47 +1000

    update copyright

--- test.go
+++ test.go
@@ -1,3 +1,8 @@
+// Copyright Gleb Obitotsky <glebobitotsky@yandex.com> 2026.
+//
+// The code is distributed under the GNU GPL v.3 license
+// (you can find the license file in the root folder).
+
 package main
 
 import (
