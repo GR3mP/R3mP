0ec33d6dcc10bb146b4febd865a04f5f896175d2
Gleb Obitotsky <pluginexteragram@gmail.com>
2026-05-03 22:55:51 +1000

    update ML-KEM and test

--- common/crypto/mlkem.go
+++ common/crypto/mlkem.go
@@ -8,14 +8,38 @@ package crypto
 import (
 	// "crypto/ecdh"
 	"crypto/mlkem"
-	// "crypto/rand"
+	"crypto/rand"
 	// "log"
 )
 
+// Func generate met, bytes ML_KEM768.
 func GenKeyMlKem() (*mlkem.DecapsulationKey768, []byte, error) {
 	dk, err := mlkem.GenerateKey768()
 	if err != nil {
 		return nil, nil, err
 	}
 	return dk, dk.Bytes(), nil
+}
+
+// Func generate 64 seed byte.
+func GenerateMlKemSeed768() ([]byte, error) {
+	seed := make([]byte, 64)
+	if _, err := rand.Read(seed); err != nil {
+		return nil, err
+	}
+	return seed, nil
+}
+
+// Func generate new decapsulation key using a 64-byte seed of the form "d || z".
+func GenDecapsulationKey(seed []byte) (*mlkem.DecapsulationKey768, error) {
+	dk, err := mlkem.NewDecapsulationKey768(seed)
+	if err != nil {
+		return nil, err
+	}
+	return dk, nil
+}
+
+// Func get 64 byte key.
+func GetKeyBytes(dk *mlkem.DecapsulationKey768) []byte {
+	return dk.Bytes()
 }
